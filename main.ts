47144686384e26d8547987a39f6871fc35ba1335
Yohan Adi Setiawan <115447721+yohanadisetiawan@users.noreply.github.com>
2023-01-31 10:16:45 +0700

    Adding new music step and sequence end game

--- main.ts
+++ main.ts
@@ -1,21 +1,44 @@
 namespace SpriteKind {
     export const NPC = SpriteKind.create()
 }
-
-controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
-    if (!ModeDialog) {
-        animation.runImageAnimation(Pemain, assets.animation`
-            PemainUp
-        `, 500, true)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(ModeDialog)) {
+        animation.runImageAnimation(
+        Pemain,
+        assets.animation`PemainUp`,
+        500,
+        true
+        )
+        music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
     }
-    
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function (sprite, otherSprite) {
     ModeDialog = true
     if (ModeDialog == true) {
-        dbSoal = ["Bagaimana cara kerja jantung dalam tubuh manusia?", "Bagaimana pandanganmu tentang teknologi canggih saat ini?", "Apa yang menyebabkan gempa bumi?", "Apakah kamu lebih suka musik klasik atau musik pop?", "Sebutkan tiga fakta tentang planet-planet dalam tata surya kita!", "Bagaimana pandanganmu tentang peran media dalam masyarakat?", "Bagaimana cara kerja otak dalam tubuh manusia?", "Apakah kamu lebih suka film Hollywood atau film Indonesia?", "Praditya berpikir teknologi canggih saat ini adalah bencana bagi masyarakat. Semua orang terlalu tergantung pada telepon pintar dan teknologi lainnya, sehingga kita kehilangan keterampilan sosial yang penting. Kita perlu kembali ke cara hidup yang lebih sederhana dan mengurangi ketergantungan pada teknologi.", "Dzakir berpikir kita harus mengurangi pemakaian plastik sebanyak mungkin. Plastik merusak lingkungan dan membahayakan hewan-hewan laut. Kita harus lebih bertanggung jawab dan memakai alternatif yang lebih ramah lingkungan."]
-        jwbSoal = ["Fakta", "Opini", "Fakta", "Opini", "Fakta", "Opini", "Fakta", "Opini", "Fakta", "Opini"]
+        dbSoal = [
+        "Bagaimana cara kerja jantung dalam tubuh manusia?",
+        "Bagaimana pandanganmu tentang teknologi canggih saat ini?",
+        "Apa yang menyebabkan gempa bumi?",
+        "Apakah kamu lebih suka musik klasik atau musik pop?",
+        "Sebutkan tiga fakta tentang planet-planet dalam tata surya kita!",
+        "Bagaimana pandanganmu tentang peran media dalam masyarakat?",
+        "Bagaimana cara kerja otak dalam tubuh manusia?",
+        "Apakah kamu lebih suka film Hollywood atau film Indonesia?",
+        "Praditya berpikir teknologi canggih saat ini adalah bencana bagi masyarakat. Semua orang terlalu tergantung pada telepon pintar dan teknologi lainnya, sehingga kita kehilangan keterampilan sosial yang penting. Kita perlu kembali ke cara hidup yang lebih sederhana dan mengurangi ketergantungan pada teknologi.",
+        "Dzakir berpikir kita harus mengurangi pemakaian plastik sebanyak mungkin. Plastik merusak lingkungan dan membahayakan hewan-hewan laut. Kita harus lebih bertanggung jawab dan memakai alternatif yang lebih ramah lingkungan."
+        ]
+        jwbSoal = [
+        "Fakta",
+        "Opini",
+        "Fakta",
+        "Opini",
+        "Fakta",
+        "Opini",
+        "Fakta",
+        "Opini",
+        "Fakta",
+        "Opini"
+        ]
         story.setSoundEnabled(true)
         game.showLongText(dbSoal[posSoal], DialogLayout.Bottom)
         pause(100)
@@ -25,116 +48,103 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function on_on_overlap(spri
         } else {
             info.changeScoreBy(-5)
         }
-        
         if (otherSprite == NPC1) {
             NPC1.destroy(effects.rings, 500)
-            NPC1 = sprites.create(assets.image`
-                Novita
-            `, SpriteKind.NPC)
+            NPC1 = sprites.create(assets.image`Novita`, SpriteKind.NPC)
             tiles.placeOnRandomTile(NPC1, sprites.dungeon.collectibleRedCrystal)
         } else {
             NPC2.destroy(effects.fountain, 500)
-            NPC2 = sprites.create(assets.image`
-                Dzakir
-            `, SpriteKind.NPC)
+            NPC2 = sprites.create(assets.image`Dzakir`, SpriteKind.NPC)
             tiles.placeOnRandomTile(NPC2, sprites.dungeon.collectibleBlueCrystal)
         }
-        
         pause(100)
         if (posSoal == dbSoal.length - 1) {
             posSoal = 0
         } else {
             posSoal += 1
         }
-        
         ModeDialog = false
     }
-    
     story.setSoundEnabled(false)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(Pemain, assets.animation`
-        PemainLeft
-    `, 500, true)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Pemain,
+    assets.animation`PemainLeft`,
+    500,
+    true
+    )
+    music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
 })
-info.onCountdownEnd(function on_countdown_end() {
+info.onCountdownEnd(function () {
+    game.setGameOverMessage(false, "PERMAINAN SELESAI!\\nAnda kalah.")
+    game.setGameOverPlayable(true, music.melodyPlayable(music.powerDown), false)
+    game.setGameOverScoringType(game.ScoringType.HighScore)
     game.gameOver(false)
-    game.setGameOverMessage(false, "PERMAINAN SELESAI! Anda kalah.")
-    game.setGameOverPlayable(false, music.melodyPlayable(music.powerDown), false)
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(Pemain, assets.animation`
-        PemainRight
-    `, 500, true)
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Pemain,
+    assets.animation`PemainRight`,
+    500,
+    true
+    )
+    music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
-    if (!ModeDialog) {
-        animation.runImageAnimation(Pemain, assets.animation`
-            PemainDown
-        `, 500, true)
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(ModeDialog)) {
+        animation.runImageAnimation(
+        Pemain,
+        assets.animation`PemainDown`,
+        500,
+        true
+        )
+        music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
     }
-    
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap2(sprite2: Sprite, otherSprite2: Sprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite2, otherSprite2) {
     game.setGameOverPlayable(true, music.melodyPlayable(music.powerUp), false)
     game.setGameOverMessage(true, "SELAMAT ANDA BERHASIL!")
     game.gameOver(true)
 })
-let Goal : Sprite = null
-let jwbSoal : string[] = []
-let dbSoal : string[] = []
+let Goal: Sprite = null
+let jwbSoal: string[] = []
+let dbSoal: string[] = []
 let posSoal = 0
 let ModeDialog = false
-let NPC2 : Sprite = null
-let NPC1 : Sprite = null
-let Pemain : Sprite = null
+let NPC2: Sprite = null
+let NPC1: Sprite = null
+let Pemain: Sprite = null
 music.play(music.createSong(hex`
         0078000408020200001c00010a006400f401640000040000000000000000000000000005000004360008000c00011b10001400011d18001c00012020002400012428002c0001202c003000011d30003400012034003800012438003c00012905001c000f0a006400f4010a00000400000000000000000000000000000000021e0008000c00010e14001800011324002800010c3400380001113c004000010c
     `), music.PlaybackMode.LoopingInBackground)
-scene.setBackgroundImage(assets.image`
-    bgImage
-`)
-game.setDialogFrame(assets.image`
-    frameDialog
-`)
+scene.setBackgroundImage(assets.image`bgImage`)
+game.setDialogFrame(assets.image`frameDialog`)
 game.splash("Game Pendidikan", "Analisis Informasi Teks")
 game.splash("Fakta dan Opini", "(c) 2023-SMP Negeri 2 Kalitidu")
 game.showLongText("Oleh:\\nYohan Adi Setiawan\\nPraditya Andi Setiawan", DialogLayout.Bottom)
-tiles.setCurrentTilemap(tilemap`
-    level1
-`)
-Pemain = sprites.create(assets.image`
-    Pandi
-`, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+Pemain = sprites.create(assets.image`Pandi`, SpriteKind.Player)
 tiles.placeOnRandomTile(Pemain, sprites.dungeon.stairLadder)
 scene.cameraFollowSprite(Pemain)
-NPC1 = sprites.create(assets.image`
-    Novita
-`, SpriteKind.NPC)
+NPC1 = sprites.create(assets.image`Novita`, SpriteKind.NPC)
 tiles.placeOnRandomTile(NPC1, sprites.dungeon.collectibleRedCrystal)
-NPC2 = sprites.create(assets.image`
-    Dzakir
-`, SpriteKind.NPC)
+NPC2 = sprites.create(assets.image`Dzakir`, SpriteKind.NPC)
 tiles.placeOnRandomTile(NPC2, sprites.dungeon.collectibleBlueCrystal)
 ModeDialog = false
 posSoal = 0
 info.setScore(0)
 info.startCountdown(120)
-forever(function on_forever() {
+forever(function () {
     if (ModeDialog == true) {
         controller.moveSprite(Pemain, 0, 0)
     } else if (ModeDialog == false) {
         controller.moveSprite(Pemain, 100, 100)
     }
-    
 })
-game.onUpdateInterval(500, function on_update_interval() {
-    
+game.onUpdateInterval(500, function () {
     if (info.score() > 70) {
-        Goal = sprites.create(assets.image`
-            FinalNPC
-        `, SpriteKind.Food)
+        Goal = sprites.create(assets.image`FinalNPC`, SpriteKind.Food)
         tiles.placeOnRandomTile(Goal, sprites.dungeon.stairNorth)
     }
-    
 })
